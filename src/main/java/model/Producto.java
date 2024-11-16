eaf7874c9c7b900408ee8fdc011a83c890b7b997
Pablo Tamayo Lopez <130286743+pablotl0@users.noreply.github.com>
2024-11-16 14:54:18 +0100

    Fix #14
    En relación a https://github.com/evaanngiil/WishfulGiving/pull/17#issuecomment-2480564420 modifico la clase Producto para añadirle un precio al producto

--- src/main/java/model/Producto.java
+++ src/main/java/model/Producto.java
@@ -1,16 +1,20 @@
 public class Producto {
     private final String nombre;
-
-    public Producto(String nombre) {
-        validarProducto(nombre);
-
+    private final double precio;  
+    
+    public Producto(String nombre, double precio) {
+        validarProducto(nombre, precio);
         this.nombre = nombre.trim();
+        this.precio = precio;
     }
 
     // Método de validación para Producto
     private void validarProducto(String nombre) {
         if (nombre == null || nombre.isBlank()) {
             throw new IllegalArgumentException("El nombre del producto no puede ser nulo o vacío.");
+        }
+        if (precio <= 0) {
+            throw new IllegalArgumentException("El precio del producto debe ser mayor a cero.");
         }
     }
 }
