eccff51352d0b3760033a3c74beaa9c9621a98db
pablotl0 <pablotl@correo.ugr.es>
2024-11-22 13:17:56 +0100

    fix #12, elimino el id de la clase al ser innecesario para gestionar la lista de elementos

--- src/ListaDeseos.java
+++ src/ListaDeseos.java
@@ -1,18 +1,10 @@
 import java.util.ArrayList;
 
 public class ListaDeseos {
-    private final long id;
     private ArrayList<Producto> deseos;
 
-    public ListaDeseos(long id) {
-        validarListaDeseos(id);
-        this.id = id;
+    public ListaDeseos() {
         this.deseos = new ArrayList<Producto>();
     }
 
-    private void validarListaDeseos(long id) {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException("El id de la lista no puede ser nulo y deber ser mayor a cero.");
-        }
-    }
 }
