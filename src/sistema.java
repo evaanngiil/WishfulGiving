8cfe2180cee3fb4646d8f603f7a4ba1879658701
Pablo Tamayo Lopez <130286743+pablotl0@users.noreply.github.com>
2024-11-28 12:52:06 +0100

    #16: Crear una clase de gestión de usuarios centralizada para mejorar la coherencia y la escalabilidad del sistema. Esta clase es necesaria para consolidar la lógica de gestión de usuarios y permitir una administración uniforme y centralizada, lo que facilitará futuras modificaciones y la integración con otros módulos.

---
+++ src/sistema.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class GestorDeUsuarios { 
+    private ArrayList<Usuario> usuarios;
+
+    public GestorDeUsuarios() {
+        this.usuarios = new ArrayList<Usuario>();	
+    }
+}
