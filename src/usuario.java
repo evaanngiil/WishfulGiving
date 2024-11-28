c622d2950708873c2d0daef53b9ed1012ccf5a60
Pablo Tamayo Lopez <130286743+pablotl0@users.noreply.github.com>
2024-11-28 11:45:55 +0100

    #12,  modifico la clase sustituyendo el objeto ListaDeseos por el vector de deseos

--- src/usuario.java
+++ src/usuario.java
@@ -1,21 +1,19 @@
+import java.util.ArrayList;
+
 public class Usuario { 
     private String nombre;
-    private ListaDeseos listaDeseos;    
+    private ArrayList<Producto> listaDeseos;
 
-    public Usuario( String nombre, ListaDeseos listaDeseos) {
-        validarUsuario(nombre, listaDeseos);
+    public Usuario(String nombre) {
+        validarUsuario(nombre);
         this.nombre = nombre.trim();
-        this.listaDeseos = listaDeseos():
+        this.listaDeseos = new ArrayList<Producto>();	
     }
 
     // Método de validación para Usuario
-    private void validarUsuario(String nombre, ListaDeseos listaDeseos) {
+    private void validarUsuario(String nombre) {
         if (nombre == null || nombre.isBlank()) {
             throw new IllegalArgumentException("El nombre del usuario no puede ser nulo o vacío.");
         }
-	    if (listaDeseos == null) {
-            throw new IllegalArgumentException("La lista de deseos no puede ser nula.");
-        }
-        
     }
 }
