09ddfdce2d3815896c38c506d49619b398d3b78a
pablotl0 <pablotl@correo.ugr.es>
2024-12-02 22:45:55 +0100

    elimino las clases para empezar de nuevo de acuerdo a la nueva HU

--- src/gestorDeUsuarios.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class GestorDeUsuarios { 
-    private HashMap<String, Usuario> usuarios;
-
-    public GestorDeUsuarios() {
-        this.usuarios = new HashMap<>();	
-    }
-}
