bae70f34350a7ab33c69fbe034092b831a811037
pablotl0 <pablotl@correo.ugr.es>
2024-12-03 19:54:10 +0100

    #14 modifico la clase Regalo para que este tenga un atributo de descripción, relevante a la hora de especificar información adicional que describa mejor el regalo. Ademas cambio el atributo nombre por titulo, todo esto con el objetivo de mejorar la representacion del problema

--- src/Regalo.java
+++ src/Regalo.java
@@ -1,11 +1,15 @@
 public class Regalo {
-    private final String nombre;
+    private final String titulo;
+    private final String descripcion;
     private final double precio;
     private final int prioridad;
 
-    public Regalo(String nombre, double precio, int prioridad;) {
-        if (nombre == null || nombre.isEmpty()) {
-            throw new IllegalArgumentException("El nombre del regalo no puede estar vacío.");
+    public Regalo(String titulo, String descripcion, double precio, int prioridad;) {
+        if (titulo == null || titulo.isEmpty()) {
+            throw new IllegalArgumentException("El titulo del regalo no puede estar vacío.");
+        }
+        if (descripcion == null || descripcion.isEmpty()) {
+            throw new IllegalArgumentException("La descripcion del regalo no puede estar vacía.");
         }
         if (precio < 0) {
             throw new IllegalArgumentException("El precio del regalo debe ser un valor positivo.");
@@ -13,7 +17,8 @@ public class Regalo {
         if (prioridad < 1 || prioridad > 5) {
                 throw new IllegalArgumentException("La prioridad debe estar entre 1 y 5.");
         }
-        this.nombre = nombre;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
         this.precio = precio;
         this.prioridad = prioridad;
     }
