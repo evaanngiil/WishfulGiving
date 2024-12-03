133ce1ddbd0c6b6c18985d81769b3558e4797167
pablotl0 <pablotl@correo.ugr.es>
2024-12-03 19:16:23 +0100

    #14 modifico la clase Regalo para que este tenga un atributo de prioridad.  La prioridad facilitará la planificación y organización, permitiendo que los regalos más relevantes o urgentes sean identificados y tratados primero. Como se ha comentado en https://github.com/evaanngiil/WishfulGiving/pull/18#discussion_r1867952954

--- src/Regalo.java
+++ src/Regalo.java
@@ -1,15 +1,20 @@
 public class Regalo {
     private final String nombre;
     private final double precio;
+    private final int prioridad;
 
-    public Regalo(String nombre, double precio) {
+    public Regalo(String nombre, double precio, int prioridad;) {
         if (nombre == null || nombre.isEmpty()) {
             throw new IllegalArgumentException("El nombre del regalo no puede estar vacío.");
         }
         if (precio < 0) {
             throw new IllegalArgumentException("El precio del regalo debe ser un valor positivo.");
         }
+        if (prioridad < 1 || prioridad > 5) {
+                throw new IllegalArgumentException("La prioridad debe estar entre 1 y 5.");
+        }
         this.nombre = nombre;
         this.precio = precio;
+        this.prioridad = prioridad;
     }
 }
