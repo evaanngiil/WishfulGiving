d76bc102a8b523faa31fcf69284e8ae45a142a09
evaanngiil <evaanngc@gmail.com>
2024-12-28 14:52:12 +0100

    Crea paquetes para poder hacer testing. No vale solo con tener una carpeta src en Java, se deben crear paquetes donde se compartan con la clase de test. Ayuda a resolver #27

---
+++ src/main/java/com/example/WishfulGiving/GestorListaRegalos.java
@@ -0,0 +1,11 @@
+package com.example.WishfulGiving;
+
+import java.util.HashMap;
+
+public class GestorListaRegalos { 
+    private HashMap<String, ListaRegalos> listas;
+
+    public GestorListaRegalos() {
+        this.listas = new HashMap<>();	
+    }
+}
