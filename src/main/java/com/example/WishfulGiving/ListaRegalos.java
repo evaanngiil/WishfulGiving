d8d4a4a753bfb257c53fdeeecf7e314db3056035
evaanngiil <evaanngc@gmail.com>
2024-12-29 23:00:50 +0100

    feat: Resuelve #5 - Elimina clase repetida

--- src/main/java/com/example/WishfulGiving/ListaRegalos.java
+++ src/main/java/com/example/WishfulGiving/ListaRegalos.java
@@ -2,8 +2,6 @@ package com.example.WishfulGiving;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.InputStream;
 
 
 import java.util.ArrayList;
@@ -105,19 +103,6 @@ public class ListaRegalos {
                 .mapToDouble(Regalo::getPrecio)
                 .sum();
         return presupuesto - totalGastado;
-    }
-    
-    public static List<ListaRegalos> cargarListasDeRegalos(String rutaArchivo) {
-        ObjectMapper mapper = new ObjectMapper();
-        
-        try (InputStream inputStream = ListaRegalos.class.getClassLoader().getResourceAsStream(rutaArchivo)) {
-            if (inputStream == null) {
-                throw new RuntimeException("Archivo no encontrado: " + rutaArchivo);
-            }
-            return List.of(mapper.readValue(inputStream, ListaRegalos[].class));
-        } catch (Exception e) {
-            throw new RuntimeException("Error al leer el archivo JSON: " + e.getMessage(), e);
-        }
     }
 }
 
