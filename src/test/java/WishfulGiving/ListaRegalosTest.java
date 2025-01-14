0765e1721f04e702b1e508d06901ec7e2329769f
Evan Gil Cortés <evaanngc@gmail.com>
2025-01-14 16:45:47 +0100

    [#29] Añade variables globales para la lista de regalos esperada. Añade test nuevo para probar la maximizacion de la satisfaccion como requiere #4.

--- src/test/java/WishfulGiving/ListaRegalosTest.java
+++ src/test/java/WishfulGiving/ListaRegalosTest.java
@@ -8,6 +8,10 @@ import java.util.List;
 public class ListaRegalosTest {
     
     private static final int REGALOS_DENTRO_PRESUPUESTO = 2;
+    private static final List<Regalo> REGALOS_ESPERADOS = List.of(
+            new Regalo("Enter de peluche", "Un enter de peluche enorme para desahogarme cuando me estreso haciendo código", 20.0, 5),
+            new Regalo("Monopoly 64 peones", "Monopoly con destacados de oro y plata y con hasta 64 peones", 70.0, 4)
+    );
     private static final String TEST_JSON_PATH = "data/test_regalos.json";
 
     @Test
@@ -17,17 +21,26 @@ public class ListaRegalosTest {
 
         List<Regalo> regalosSeleccionados = listaJuan.obtenerRegalosDentroPresupuesto();
 
-        List<Regalo> regalosEsperados = List.of(
-                new Regalo("Enter de peluche", "Un enter de peluche enorme para desahogarme cuando me estreso haciendo código", 20.0, 5),
-                new Regalo("Libro", "Un libro sobre Testing en Java super intersante que en algun lugar de la mancha", 20.0, 3)
-        );
-
-        boolean sonIguales = regalosEsperados.size() == regalosSeleccionados.size()
-                && regalosEsperados.stream().allMatch(esperado
+        boolean sonIguales = REGALOS_ESPERADOS.size() == regalosSeleccionados.size()
+                && REGALOS_ESPERADOS.stream().allMatch(esperado
                         -> regalosSeleccionados.stream().anyMatch(seleccionado
                         -> seleccionado.getTitulo().equals(esperado.getTitulo())));
-
+        
         assertTrue(sonIguales, "Los regalos seleccionados no coinciden exactamente con los regalos esperados.");
+    }
+
+    @Test
+    void maximizarSatisfaccion() {
+        List<ListaRegalos> listas = JsonLoader.cargarListasDeRegalos(TEST_JSON_PATH);
+        ListaRegalos lista = listas.get(0);
+
+        List<Regalo> regalosSeleccionados = lista.obtenerRegalosDentroPresupuesto();
+
+        int prioridadSeleccionada = regalosSeleccionados.stream().mapToInt(Regalo::getPrioridad).sum();
+        int prioridadEsperada = REGALOS_ESPERADOS.stream().mapToInt(Regalo::getPrioridad).sum();
+
+        assertEquals(prioridadEsperada, prioridadSeleccionada, "La prioridad total calculada no maximiza la satisfacción dentro del presupuesto.");
+        assertTrue(regalosSeleccionados.size() <= REGALOS_DENTRO_PRESUPUESTO, "El número de regalos seleccionados no debe exceder los disponibles dentro del presupuesto.");
     }
     
     @Test
@@ -40,5 +53,4 @@ public class ListaRegalosTest {
         assertEquals(10.0, presupuestoRestante,
                 "El presupuesto restante calculado no es el esperado.");
     }
-
-    
+}
